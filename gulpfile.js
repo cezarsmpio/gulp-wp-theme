c81b7605c98f38e0238d8999152d2fd91ed57d60
Cezar Luiz <cezarluiz.c@gmail.com>
2016-07-29 00:45:03 -0300

    Changed the config path and added ES6 template string support

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,13 @@ var config      = require('./gulp.config.js');
 
 // Tasks
 gulp.task('sass', function () {
-  return gulp.src(config.development_path + 'sass/**/*.{scss,sass}')
+  return gulp.src(`${config.development_path}/sass/**/*.{scss,sass}`)
     .pipe($.plumber())
     .pipe($.sass({
       includePaths: [
         'node_modules',
         'bower_components',
-         config.development_path + 'sass'
+         `${config.development_path}/sass`
       ]
     }))
     .pipe($.autoprefixer({
@@ -23,11 +23,11 @@ gulp.task('sass', function () {
     }))
     .pipe($.concat('style.css'))
     .pipe($.cssnano())
-    .pipe(gulp.dest(config.tmp_path + 'wordpress/wp-content/themes/' + config.theme_path));
+    .pipe(gulp.dest(`${config.tmp_path}/wordpress/wp-content/themes/${config.theme_path}`));
 });
 
 gulp.task('scripts', function () {
-  return gulp.src(config.development_path + 'js/main.js')
+  return gulp.src(`${config.development_path}/js/main.js`)
     .pipe($.plumber())
     .pipe($.babel({
       presets: ['es2015']
@@ -37,17 +37,17 @@ gulp.task('scripts', function () {
       includePaths: [
         'node_modules',
         'bower_components',
-         config.development_path + 'js'
+         `${config.development_path}/js`
       ]
     }))
     .pipe($.uglify({
       mangle: true
     }))
-    .pipe(gulp.dest(config.tmp_path + 'wordpress/wp-content/themes/' + config.theme_path + '/js'));
+    .pipe(gulp.dest(`${config.tmp_path}/wordpress/wp-content/themes/${config.theme_path}/js`));
 });
 
 gulp.task('scripts:components', function () {
-  return gulp.src(config.development_path + 'js/components/**/*.js')
+  return gulp.src(`${config.development_path}/js/components/**/*.js`)
     .pipe($.plumber())
     .pipe($.babel({
       presets: ['es2015']
@@ -57,40 +57,40 @@ gulp.task('scripts:components', function () {
       includePaths: [
         'node_modules',
         'bower_components',
-         config.development_path + 'js'
+         `${config.development_path}/js`
       ]
     }))
     .pipe($.uglify({
       mangle: true
     }))
-    .pipe(gulp.dest(config.tmp_path + 'wordpress/wp-content/themes/' + config.theme_path + '/js/components'));
+    .pipe(gulp.dest(`${config.tmp_path}/wordpress/wp-content/themes/${config.theme_path}/js/components`));
 });
 
 gulp.task('scripts:vendor', function () {
-  return gulp.src(config.development_path + 'js/vendor/**/*.js')
+  return gulp.src(`${config.development_path}/js/vendor/**/*.js`)
     .pipe($.plumber())
     .pipe($.concat('vendor.min.js'))
     .pipe($.uglify({
       mangle: true
     }))
-    .pipe(gulp.dest(config.tmp_path + 'wordpress/wp-content/themes/' + config.theme_path + '/js/vendor'));
+    .pipe(gulp.dest(`${config.tmp_path}/wordpress/wp-content/themes/${config.theme_path}/js/vendor`));
 });
 
 gulp.task('images', function () {
-  return gulp.src(config.development_path + 'images/**/*')
+  return gulp.src(`${config.development_path}/images/**/*`)
     .pipe($.cache($.imagemin()))
-    .pipe(gulp.dest(config.tmp_path + 'wordpress/wp-content/themes/' + config.theme_path + '/images'));
+    .pipe(gulp.dest(`${config.tmp_path}/wordpress/wp-content/themes/${config.theme_path}/images`));
 });
 
 gulp.task('fonts', () => {
-  return gulp.src(config.development_path + 'fonts/**/*')
-    .pipe(gulp.dest(config.tmp_path + 'fonts'));
+  return gulp.src(`${config.development_path}/fonts/**/*`)
+    .pipe(gulp.dest(`${config.tmp_path}/fonts`));
 });
 
 gulp.task('php', ['sass', 'scripts', 'scripts:components', 'scripts:vendor', 'images', 'fonts'], function () {
-  return gulp.src(config.development_path + '**/*.php')
+  return gulp.src(`${config.development_path}/**/*.php`)
     .pipe($.plumber())
-    .pipe(gulp.dest(config.tmp_path + 'wordpress/wp-content/themes/' + config.theme_path));
+    .pipe(gulp.dest(`${config.tmp_path}/wordpress/wp-content/themes/${config.theme_path}`));
 });
 
 gulp.task('wordpress', function () {
@@ -101,11 +101,11 @@ gulp.task('wordpress', function () {
 
 gulp.task('screenshot', function () {
   return gulp.src('screenshot.png')
-    .pipe(gulp.dest(config.tmp_path + 'wordpress/wp-content/themes/' + config.theme_path));
+    .pipe(gulp.dest(`${config.tmp_path}/wordpress/wp-content/themes/${config.theme_path}`));
 });
 
 gulp.task('build', ['php'], function () {
-  return gulp.src(config.tmp_path + 'wordpress/**/*')
+  return gulp.src(`${config.tmp_path}/wordpress/**/*`)
     .pipe(gulp.dest(config.dist_path));
 });
 
@@ -115,7 +115,7 @@ gulp.task('default', function () {
 
 gulp.task('watch', ['php', 'screenshot'], function () {
   $.connectPhp.server({
-    base:  config.tmp_path + 'wordpress'
+    base:  `${config.tmp_path}/wordpress`
   }, function () {
     browserSync({
       proxy: '127.0.0.1:8000',
@@ -123,16 +123,16 @@ gulp.task('watch', ['php', 'screenshot'], function () {
     });
   });
 
-  gulp.watch(config.development_path + 'images/**/*', ['images']);
-  gulp.watch(config.development_path + 'fonts/**/*', ['fonts']);
-  gulp.watch(config.development_path + 'js/main.js', ['scripts']);
-  gulp.watch(config.development_path + 'js/components/**/*.js', ['scripts:components']);
-  gulp.watch(config.development_path + 'js/vendor/**/*.js', ['scripts:vendor']);
-  gulp.watch(config.development_path + 'sass/**/*.{scss,sass}', ['sass']);
+  gulp.watch(`${config.development_path}/images/**/*`, ['images']);
+  gulp.watch(`${config.development_path}/fonts/**/*`, ['fonts']);
+  gulp.watch(`${config.development_path}/js/main.js`, ['scripts']);
+  gulp.watch(`${config.development_path}/js/components/**/*.js`, ['scripts:components']);
+  gulp.watch(`${config.development_path}/js/vendor/**/*.js`, ['scripts:vendor']);
+  gulp.watch(`${config.development_path}/sass/**/*.{scss,sass}`, ['sass']);
 
-  gulp.watch(config.development_path + '**/*.php', ['php']);
+  gulp.watch(`${config.development_path}/**/*.php`, ['php']);
 
-  gulp.watch(config.development_path + '**/*').on('change', function () {
+  gulp.watch(`${config.development_path}/**/*').on('change`, function () {
     reload();
   });
 });
